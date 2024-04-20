e43de3bd67f37232762ac0d2edd2b6855abda58c
unknown <venkateshgolla134@gmail.com>
2024-04-20 15:34:45 +0530

    all

--- assets/component-splide.js
+++ assets/component-splide.js
@@ -1,57 +1,46 @@
-if(!customElements.get("custom-slider")) {
-class CustomSlider extends HTMLElement{
+if (!customElements.get("custom-slider")) {
+   class CustomSlider extends HTMLElement {
+       constructor() {
+           super();
+           this.element = this;
+           console.log(this);
+           this.options = JSON.parse(this.element.dataset.sliderOptions);
+           console.log(this.options);
 
-   constructor(){
-      super();
-      this.element = this
-      this.options = JSON.parse(this.element.dataset.sliderOptions);
-   
+           this.displaySliderShow();
+       }
 
-      this.displaySliderShow()
+       displaySliderShow() {
+           let reviews = new Splide(this.element, this.options).mount();
+           let imageSlide = new Splide("#display-image", {
+               type: "fade",
+               perPage: 1,
+               pagination: false,
+               arrows: false
+           }).mount();
+           reviews.sync(imageSlide);
 
-   }
-
-    displaySliderShow(){
-      let reviews = new Splide(this.element, this.options).mount();
-            let imageSlide = new Splide("#display-image", {
-                type: "fade",
-                perPage: 1,
-                pagination: false,
-                arrows: false
-            }).mount();
-            reviews.sync(imageSlide);
-
-            reviews.on("active", (slide) => { // Use arrow function to retain the context of `this`
+           reviews.on("active", (slide) => { // Use arrow function to retain the context of `this`
                var activeIndex = reviews.index;
-               if (activeIndex ===0 ){
-                  var color = slide.dataset.active 
-               }
-              try{
-               if (slide !== undefined){
-                  console.log("active slide")
-               
-                   
-                  
-                  
-               }
-               else {
-                  console.log("not found")
-               }
-
-              }
-              catch(e){
-               console.log("slide not found")
-              }
+               console.log(activeIndex);
+               this.toggleActiveClass(reviews.Components.Elements.slides, activeIndex);
            });
 
+           this.reviews = reviews; // Store reviews instance as a property of the class
+
            // Trigger the "active" event for the initial slide
-         //   reviews.emit("active", reviews.Components.Elements.slides[0]);
+           reviews.emit("active", reviews.Components.Elements.slides[0]);
+       }
 
-
+       toggleActiveClass(slides, activeIndex) {
+           slides.forEach((slide, index) => {
+               if (index === activeIndex) {
+                   slide.classList.add("active-slide");
+               } else {
+                   slide.classList.remove("active-slide");
+               }
+           });
+       }
+   }
+   customElements.define("custom-slider", CustomSlider);
 }
-}
-  customElements.define("custom-slider", CustomSlider)
-
-}
-
-  
