387f6ba59d244599249e96aec376d9a5dacddaba
unknown <venkateshgolla134@gmail.com>
2024-04-22 17:29:48 +0530

    all

--- assets/component-splide.js
+++ assets/component-splide.js
@@ -1,45 +1,41 @@
 if (!customElements.get("custom-slider")) {
-   class CustomSlider extends HTMLElement {
-       constructor() {
-           super();
-           this.element = this;
-           this.options = JSON.parse(this.element.dataset.sliderOptions)
-           this.displaySliderShow();
-       }
+    class CustomSlider extends HTMLElement {
+        constructor() {
+            super();
+            this.element = this;
+            this.options = JSON.parse(this.element.dataset.sliderOptions)
+            this.displaySliderShow();
+        }
 
-       displaySliderShow() {
-           let reviews = new Splide(this.element, this.options).mount();
-           let imageSlide = new Splide("#display-image", {
-               type: "fade",
-               perPage: 1,
-               pagination: false,
-               arrows: false
-           }).mount();
-           reviews.sync(imageSlide);
-           reviews.on("active", (slide) => { 
-               var activeIndex = reviews.index;
-               this.toggleActiveClass(reviews.Components.Elements.slides, activeIndex);
-           });
+        displaySliderShow() {
+            let reviews = new Splide(this.element, this.options).mount();
+            let imageSlide = new Splide("#display-image", {
+                type: "fade",
+                perPage: 1,
+                pagination: false,
+                arrows: false
+            }).mount();
+            reviews.sync(imageSlide);
 
-           this.reviews = reviews; 
-           reviews.emit("active", reviews.Components.Elements.slides[0]);
-       }
+            reviews.on("move", (newIndex, oldIndex, destIndex) => {
+                this.toggleActiveClass(reviews.Components.Elements.slides, newIndex);
+            });
 
-       toggleActiveClass(slides, activeIndex) {
-           slides.forEach((slide, index) => {
-               if (index === activeIndex) {
-                slide.querySelector(".custom-testimonials--slide").classList.remove(`normal-color-${activeIndex}`)
-                slide.querySelector(".custom-testimonials--slide").classList.add(`active-slide-${activeIndex}`);
-                console.log(`active-slide-${activeIndex}`)
-                  
-               } else {
-                slide.querySelector(".custom-testimonials--slide").classList.remove(`active-slide-${activeIndex}`);
-                slide.querySelector(".custom-testimonials--slide").classList.add(`normal-color-${activeIndex}`)
-                console.log(`normal-color-${activeIndex}`)
-                console.log(`active-slide-${activeIndex}`)
-               }
-           });
-       }
-   }
-   customElements.define("custom-slider", CustomSlider);
+            this.reviews = reviews;
+            this.toggleActiveClass(reviews.Components.Elements.slides, 0); // Apply initial colors
+        }
+
+        toggleActiveClass(slides, activeIndex) {
+            slides.forEach((slide, index) => {
+                if (index === activeIndex) {
+                    slide.querySelector(".custom-testimonials--slide").classList.add(`active-slide-${activeIndex}`);
+                    slide.querySelector(".custom-testimonials--slide").classList.remove(`normal-color-${index}`);
+                } else {
+                    slide.querySelector(".custom-testimonials--slide").classList.remove(`active-slide-${index}`);
+                    slide.querySelector(".custom-testimonials--slide").classList.add(`normal-color-${index}`);
+                }
+            });
+        }
+    }
+    customElements.define("custom-slider", CustomSlider);
 }
